2f8260283a713e8ac8aa5ab3c043a8844dabf4a9
Francisco Todaro <palitotodaro100@gmail.com>
2022-10-27 20:39:00 -0300

    subir proyecto

---
+++ pali.js
@@ -0,0 +1,120 @@
+
+
+class Producto {
+    constructor(id, nombre, pais, precio, img) {
+        this.id = id;
+        this.nombre = nombre; 
+        this.pais = pais;
+        this.precio = precio;
+        this.img = img;
+        this.cantidad = 1; 
+    }
+}
+
+const dimaria = new Producto(1, "Angel Di Maria", "Argentina", 1500, "img/dimaria.png");
+const papu = new Producto(2, "Papu Gomez","Argentina", 1000, "img/papugomez.png");
+const messi = new Producto(3, "Leonel Messi", "Argentina", 3500, "img/messi.jpg");
+const cuty = new Producto(4, "Cristian Romero","Argentina", 1000, "img/cuty.png");
+const neymar = new Producto(5, "Neymar JR","Brasil", 2500, "img/neymar.png");
+const vinicius = new Producto(6, "Vinicius JR","Brasil", 1500, "img/vinijr.png");
+const militao = new Producto(7, "Militao","Brasil", 500, "img/militao.png");
+const antony = new Producto(8, "Antony","Brasil", 1000, "img/antony.png");
+const kante = new Producto(9, "Ngolo Kante","Francia",2000, "img/kante.png");
+const griezman = new Producto(10, "Antoine Griezman","Francia", 1500, "img/griezman.png");
+const benzema = new Producto(11, "Karim Benzema","Francia", 2400, "img/benzema.png");
+const pogba = new Producto(12, "Paul Pogba","Francia", 1500, "img/pogba.jpg");
+const renato = new Producto(13,"Renato Sanches","Portugal", 500,  "img/renatosanches.png");
+const neves = new Producto(14, "Neves","Portugal",  1000, "img/neves.png");
+const cristiano = new Producto(15,"Cristiano Ronaldo","Portugal", 3000,  "img/cr7.png");
+const mendes = new Producto(16, "Mendes","Portugal",  5000, "img/mendes.png");
+
+// creo el array
+const productos = [dimaria, papu, messi, cuty, neymar, vinicius, militao, antony, kante, griezman, benzema, pogba, renato, neves, cristiano, mendes];
+
+// hago el array del carrito vacio 
+
+let carrito = [];
+
+ const contenedorProductos = document.getElementById("contenedorProductos");
+
+// cree la funcion para mostrar los productos
+
+const mostrarProductos = () => {
+    productos.forEach((producto) => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
+            <div class="card">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+                <div class="card-body">
+                <h4 class="card-title"> ${producto.nombre} </h4>
+                <h5 class="card-title"> ${producto.pais} </h5>
+                <p class="card-text"> ${producto.precio} </p>
+                <button class="btn colorBoton" id="boton${producto.id}"> Agregar al Carrito </button>
+                </div>
+            </div>
+        `
+        contenedorProductos.appendChild(card);
+
+        //se agregan productos al carrito 
+        const boton = document.getElementById(`boton${producto.id}`);
+        boton.addEventListener("click", () => {
+            agregarAlCarrito(producto.id)
+        })
+    })
+}
+
+//fruncion agregar carrito 
+
+const agregarAlCarrito = (id) => {
+    const producto = productos.find((producto) => producto.id === id);
+    const productoEnCarrito = carrito.find((producto) => producto.id === id);
+    if(productoEnCarrito){
+        productoEnCarrito.cantidad++;
+    }else {
+        carrito.push(producto);
+    
+        localStorage.setItem("carrito",JSON.stringify(carrito));
+    }
+    calcularTotal();
+}
+
+mostrarProductos();
+
+//mostar carrito
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+
+const verCarrito = document.getElementById("verCarrito");
+
+verCarrito.addEventListener("click", () => {
+    mostrarCarrito();
+});
+
+
+
+const mostrarCarrito = () => {
+    contenedorCarrito.innerHTML="";
+    carrito.forEach((producto) => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+        card.innerHTML = `
+            <div class="card">
+                <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+                <div class="card-body">
+                <h4 class="card-title"> ${producto.nombre} </h4>
+                <h5 class="card-title"> ${producto.pais} </h5>
+                <p class="card-text"> ${producto.precio} </p>
+                <p class="card-text"> ${producto.cantidad} </p>
+                <button class="btn colorBoton" id="eliminar${producto.id}"> Eliminar Producto </button>
+                </div>
+            </div>
+        `
+        contenedorCarrito.appendChild(card);
+
+       
+    })
+    calcularTotal();
+}
+
+
